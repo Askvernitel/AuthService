d6c94ccd3476e4b1a8f6e41450cd90fd35162243
Askvernitel <danielkolotashvilio@gmail.com>
2025-05-20 09:10:55 +0400

    Auth Grpc Implementation change

--- judge-auth/src/main/java/com/judge/auth/grpc/AuthServiceImpl.java
+++ judge-auth/src/main/java/com/judge/auth/grpc/AuthServiceImpl.java
@@ -19,24 +19,27 @@ public class AuthServiceImpl extends AuthServiceImplBase {
   AuthController authController;
   @Autowired
   JWTService jwtService;
-  private void handleErrorResponse(StreamObserver<AuthResponse> responseObserver){
+
+  private void handleErrorResponse(StreamObserver<AuthResponse> responseObserver) {
     AuthResponse auth = AuthResponse.newBuilder().setOk(false).build();
     responseObserver.onNext(auth);
     responseObserver.onCompleted();
   }
-  private void handleSuccessResponse(StreamObserver<AuthResponse> responseObserver){
-      AuthResponse auth = AuthResponse.newBuilder().setOk(true).build();
-      responseObserver.onNext(auth);
-      responseObserver.onCompleted();
+
+  private void handleSuccessResponse(StreamObserver<AuthResponse> responseObserver) {
+    AuthResponse auth = AuthResponse.newBuilder().setOk(true).build();
+    responseObserver.onNext(auth);
+    responseObserver.onCompleted();
   }
+
   @Override
   public void auth(AuthRequest request, StreamObserver<AuthResponse> responseObserver) {
-    //authController.login()
+    // authController.login()
     String token = request.getToken();
     try {
       jwtService.isValidJWT(token);
       System.out.println("Cool It Is valid ");
-    }catch(Exception e){
+    } catch (Exception e) {
       System.out.println(e.getMessage());
       System.out.println("NOT VALID :(");
       handleErrorResponse(responseObserver);
